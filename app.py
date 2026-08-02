4c3a28e6578ddb985ebd24da36683de3a4f6b379
Devesh Pandey <devpandey7122@gmail.com>
2026-08-02 17:43:50 +0530

    Add QR code image upload feature

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if "qr_image" not in request.files:
+        return "No file selected."
+
+    file = request.files["qr_image"]
+
+    if file.filename == "":
+        return "No file selected."
+
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+
+    return "QR Code uploaded successfully!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
